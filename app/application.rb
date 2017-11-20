6895bede21427b7bc11bc6f11b17dd0a540f9499
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-20 15:11:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,29 @@
-require 'time'
-
 class Application
-
+ 
+  @@items = ["Apples","Carrots","Pears"]
+ 
   def call(env)
-
     resp = Rack::Response.new
-    time = Time.now
-
-    if time.hour < 12
-      resp.write "Good Morning!"
+    req = Rack::Request.new(env)
+ 
+    if req.path.match(/items/)
+      @@items.each do |item|
+        resp.write "#{item}\n"
+      end
+    elsif req.path.match(/search/)
+ 
+      search_term = req.params["q"]
+ 
+      if @@items.include?(search_term)
+        resp.write "#{search_term} is one of our items"
+      else
+        resp.write "Couldn't find #{search_term}"
+      end
+ 
     else
-      resp.write "Good Afternoon"
+      resp.write "Path Not Found"
     end
-
+ 
     resp.finish
   end
 end
